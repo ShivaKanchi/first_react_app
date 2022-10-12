adaa9583d49fb0c9c307ccc5c362f24498b9af24
ShivaKanchi <shivakanchi111@gmail.com>
2022-10-12 21:12:20 +0530

    Not working

--- src/components/Functioncomponenet.jsx
+++ src/components/Functioncomponenet.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 
 function Functioncomponent() {
-    const [count, setCount] = useState(0);
+    const Functioncomponent = (props) => {
+        const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <p>This is Functional Component</p>
-            <button onClick={() => setCount(count + 1)}>Click me to increase count</button>
-            <button onClick={() => setCount(count - 1)}>Click me to decrease count</button>
-            <h1>{count}</h1>
-        </div>
-    );
-}
+        return (
+            <div>
+                <p>This is Functional Component</p>
+                <button onClick={() => setCount(count + 1)}>Click me to increase count</button>
+                <button onClick={() => setCount(count - 1)}>Click me to decrease count</button>
+                <h1>{count}</h1>
+            </div>
+        );
+    }
+};
 
 export default Functioncomponent;
